dbfd2d71d72cde753c698d82515dcd34a4f3278c
Miso Kim <kimms9725@naver.com>
2024-11-21 11:01:03 +0900

    refactor: 페이지 추가 함수 분리

--- nextjs-todo/src/features/layout/SideBar.tsx
+++ nextjs-todo/src/features/layout/SideBar.tsx
@@ -1,53 +1,18 @@
 'use client';
 
-import { useAtom } from 'jotai';
-
 import { SearchBar, CustomButton } from '@/components';
 import { PageList } from './PageList';
-import { pagesAtom } from '@/store';
-import { useRouter } from 'next/navigation';
-import { supabase } from '@/utils/supabase';
-import { useToast } from '@/hooks/use-toast';
+import { useCreatePage } from '@/shared/api';
 
 function SideBar() {
-  const [pages, setPages] = useAtom(pagesAtom);
-  const router = useRouter();
-  const { toast } = useToast();
-
-  const onAddPage = async () => {
-    try {
-      const { data, status, error } = await supabase
-        .from('todos')
-        .insert([{ title: '', boards: [], from: null, to: null }])
-        .select();
-
-      if (status === 201 && data) {
-        setPages([...pages, data[0]]);
-        toast({
-          title: '페이지 생성이 완료되었습니다.',
-          description: `/boards/${data[0].id}`,
-        });
-        router.push(`/boards/${data[0].id}`);
-      }
-
-      if (error) {
-        toast({
-          variant: 'destructive',
-          title: '에러가 발생했습니다.',
-          description: '개발자 도구 창을 확인하세요.',
-        });
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  };
+  const [createPage] = useCreatePage();
 
   return (
     <div className="w-full h-full flex flex-col justify-start items-center gap-4 ">
       {/* 검색창 영역 */}
       <SearchBar placeholder="검색어를 입력하세요." className="w-full" />
       {/* 버튼 */}
-      <CustomButton type="secondary" className="w-full" onClick={onAddPage}>
+      <CustomButton type="secondary" className="w-full" onClick={createPage}>
         Add New Page
       </CustomButton>
       {/* 페이지 컨테이너 영역 */}
--- nextjs-todo/src/shared/api/index.ts
+++ nextjs-todo/src/shared/api/index.ts
@@ -1,56 +1,41 @@
+import { useToast } from '@/hooks/use-toast';
+import { pagesAtom } from '@/store';
 import { supabase } from '@/utils/supabase';
+import { useAtom } from 'jotai';
+import { useRouter } from 'next/navigation';
 
-export const fetchAllPages = async () => {
-  try {
-    const { data, status } = await supabase
-      .from('todos')
-      .select('*')
-      .order('created_at', { ascending: true });
+export const useCreatePage = () => {
+  const [pages, setPages] = useAtom(pagesAtom);
+  const { toast } = useToast();
+  const router = useRouter();
 
-    if (status === 200 && data) {
-      return data;
+  const addPage = async () => {
+    try {
+      const { data, status, error } = await supabase
+        .from('todos')
+        .insert([{ title: '', boards: [], from: null, to: null }])
+        .select();
+
+      if (status === 201 && data) {
+        setPages([...pages, data[0]]);
+        toast({
+          title: '페이지 생성이 완료되었습니다.',
+          description: `/boards/${data[0].id}`,
+        });
+        router.push(`/boards/${data[0].id}`);
+      }
+
+      if (error) {
+        toast({
+          variant: 'destructive',
+          title: '에러가 발생했습니다.',
+          description: '개발자 도구 창을 확인하세요.',
+        });
+      }
+    } catch (error) {
+      console.error(error);
     }
-  } catch (error) {
-    console.error(error);
-  }
-};
+  };
 
-export const fetchPageById = async (id: number) => {
-  try {
-    const { data, status } = await supabase.from('todos').select().eq('id', id);
-
-    if (status === 200 && data) {
-      return data[0];
-    }
-  } catch (error) {
-    console.error(error);
-  }
-};
-
-export const createPage = async () => {
-  try {
-    const { data, status, error } = await supabase
-      .from('todos')
-      .insert([{ title: '', boards: [], from: null, to: null }])
-      .select();
-
-    if (status === 201 && data) {
-      setPages([...pages, data[0]]);
-      toast({
-        title: '페이지 생성이 완료되었습니다.',
-        description: `/boards/${data[0].id}`,
-      });
-      router.push(`/boards/${data[0].id}`);
-    }
-
-    if (error) {
-      toast({
-        variant: 'destructive',
-        title: '에러가 발생했습니다.',
-        description: '개발자 도구 창을 확인하세요.',
-      });
-    }
-  } catch (error) {
-    console.error(error);
-  }
+  return [addPage];
 };
