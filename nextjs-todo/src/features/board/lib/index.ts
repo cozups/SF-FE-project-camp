3851613b888e1f30c64d8302058fbd1c3fba07a1
Miso Kim <kimms9725@naver.com>
2024-11-28 15:15:28 +0900

    build: 배포 에러 fix

--- nextjs-todo/src/features/board/lib/index.ts
+++ nextjs-todo/src/features/board/lib/index.ts
@@ -1,24 +1,3 @@
-import { useAtom } from 'jotai';
-import { nanoid } from 'nanoid';
-
-import { BoardData } from '@/app/types';
-import { currentPageAtom } from '@/store';
-
-export const onAddBoard = async () => {
-  const [currentPage, setCurrentPage] = useAtom(currentPageAtom);
-  const newBoards: BoardData[] = [...(currentPage.boards || [])];
-  const boardContent = {
-    id: nanoid(8),
-    isCompleted: false,
-    title: '',
-    from: null,
-    to: null,
-    contents: '',
-  };
-  newBoards.push(boardContent);
-  setCurrentPage({ ...currentPage, boards: newBoards });
-};
-
 export const calculateTimeOffset = (date: Date) => {
   const offsetInMinutes = date.getTimezoneOffset();
   const calculatedTime = new Date(date.getTime() - offsetInMinutes * 60 * 1000);
